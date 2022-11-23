1f23522f9f9dfcd0061873d8f79dfd51f281fb2a
Anush <anushkumar.s@rently.com>
2022-11-23 11:34:15 +0530

    demo

--- src/agent_handoff.js
+++ src/agent_handoff.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
 export default function(message, user){
-    const socket2 = io('http://localhost:5000');
+    const socket2 = io('http://3.234.144.111:5000');
 
     socket2.emit('event-1', {user: user, message: message})
     console.log('emitted')
